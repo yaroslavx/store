5ca8efaa1b32e00f7e5ab754f5e41c48bc32cf71
yaroslavx <yakupov.yaroslav@icloud.com>
2025-08-31 17:42:59 +0300

    MAINTAIN: Seeded data

--- Store.Api/Data/Migrations/GameStoreContextModelSnapshot.cs
+++ Store.Api/Data/Migrations/GameStoreContextModelSnapshot.cs
@@ -56,6 +56,33 @@ namespace Store.Api.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Genres");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Name = "Fighting"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Name = "Roleplaying"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Name = "Sports"
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Name = "Racing"
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Name = "Kids and Family"
+                        });
                 });
 
             modelBuilder.Entity("Store.Api.Entities.Game", b =>
